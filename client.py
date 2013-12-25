61c4ece1d693f6820ed5e44bc561a190dae7fadc
Jukka Pietilä <jukkapietila@gmail.com>
2013-12-25 02:57:07 +0200

    More testing

--- client.py
+++ client.py
@@ -2,9 +2,10 @@ import feedparser
 
 d = feedparser.parse("http://feeds.feedburner.com/RockPaperShotgun.xml")
 
-#print "Elements: " + len([d.entries])
+# print "Items: " + len(d['items'])
 print "Title: " + d.entries[0].title
 print "Link: " + d.entries[0].link
 print "Desc: " + d.entries[0].description.replace("&#8217;", "\'")
 print "Published: " + d.entries[0].published
+print "Updated: " + d.entries[0].updated
 print "Id " + d.entries[0].id
